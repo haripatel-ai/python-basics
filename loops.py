9d4224d032f6c98832122694ef553b1ffcffa76f
haripatel-ai <haripatel2334@gmail.com>
2026-01-22 18:57:15 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,8 +1,27 @@
-# Day 4: Loops
-for i in range(1, 6):
-    print("For Loop:", i)
+- Loop Practice
 
-count = 1
-while count <= 5:
-    print("While Loop:", count)
-    count += 1
+# 1. Print numbers from 1 to 10
+for i in range(1, 11):
+    print(i)
+
+print("------------")
+
+# 2. Print even numbers from 1 to 20
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+
+print("------------")
+
+# 3. Sum of first 5 numbers
+sum = 0
+for i in range(1, 6):
+    sum += i
+print("Sum is:", sum)
+
+print("------------")
+
+# 4. Multiplication table of 5
+num = 5
+for i in range(1, 11):
+    print(num, "x", i, "=", num * i)
